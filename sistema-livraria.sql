8376416c5a05f6e0ef1cec5fbddaf7addcb240be
milenabasiliosilva-dot <milena.basilio.silva@escola.pr.gov.br>
2026-06-26 11:17:31 -0300

    Update sistema-livraria.sql

--- sistema-livraria.sql
+++ sistema-livraria.sql
@@ -12,3 +12,42 @@ CREATE TABLE Compras (
     NomeLivro VARCHAR(150) NOT NULL,
     FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
 );
+
+SELECT * FROM Clientes; 
+
+
+-- Inserindo clientes
+INSERT INTO Clientes (nomeCliente, emailCliente)
+VALUES 
+    ('Ana Silva', 'ana.silva@email.com'),
+    ('Carlos Souza', 'carlos.souza@email.com'),
+    ('Mariana Oliveira', 'mariana.oliveira@email.com'),
+    ('João Pereira', 'joao.pereira@email.com');
+
+-- Inserindo compras
+INSERT INTO Compras (ClienteID, NomeLivro)
+VALUES 
+    (1, 'Dom Casmurro'),
+    (2, 'O Pequeno Príncipe'),
+    (1, '1984'),
+    (3, 'A Revolução dos Bichos'),
+    (4, 'Memórias Póstumas de Brás Cubas');
+
+
+-- Inserindo clientes
+INSERT INTO Clientes (nomeCliente, emailCliente)
+VALUES 
+    ('Ana Silva', 'ana.silva@email.com'),
+    ('Carlos Souza', 'carlos.souza@email.com'),
+    ('Mariana Oliveira', 'mariana.oliveira@email.com'),
+    ('João Pereira', 'joao.pereira@email.com');
+
+-- Inserindo compras
+INSERT INTO Compras (ClienteID, NomeLivro)
+VALUES 
+    (1, 'Dom Casmurro'),
+    (2, 'O Pequeno Príncipe'),
+    (1, '1984'),
+    (3, 'A Revolução dos Bichos'),
+    (4, 'Memórias Póstumas de Brás Cubas');
+
